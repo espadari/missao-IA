7f9b30e70be01fdf85d1baa3ffb1b26d86f288ec
espadari <108538086+espadari@users.noreply.github.com>
2024-08-16 12:20:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,54 @@ const caixaPrincipal = document.querySelector(".caixa-resultado");
 const caixaPrincipal = document.querySelector(".caixa-resultado");
 
 //Array de objetos contendo as perguntas e alernativas
-const perguntas = {
+const perguntas = [
+    {
+        enunciado: "Qual a marca líder em dispositivos?"
+    alternativas: [
+            "samsung",
+            "apple"
+        ],
+        correta: 0 // A segunda alternativa é a correta
+    },
+    {
+        enunciado: "Qual região do Brasil é mais quente?"
+    alternativas: [
+            "Norte",
+            "Nordeste"
+        ],
+        correta: 0 // A segunda alternativa é a correta
+    },
+    {
+        enunciado: "Qual a comida mais típica do Brasil?"
+    alternativas: [
+            "açaí",
+            "feijoada"
+        ],
+        correta: 0 // A segunda alternativa é a correta
+    },
+    {
+        enunciado: "Qual a região mais fria do Brasil?"
+    alternativas: [
+            "Sul",
+            "Sudeste"
+        ],
+        correta: 1 // A segunda alternativa é a correta
+    },
+    {
+        enunciado: "Qual o país mais populoso?"
+    alternativas: [
+            "China",
+            "japão"
+        ],
+        correta: 1 // A segunda alternativa é a correta
+    },
 
-}
+
+
+
+
+
+
+
+
+]
