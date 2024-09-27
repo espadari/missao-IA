bbc23596dc09c2812308fc1474f7c25f7129d72f
espadari <108538086+espadari@users.noreply.github.com>
2024-09-27 12:14:38 -0300

    f

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPrincipal = document.querySelector(".caixa-pergunta");
-const caixaPrincipal = document.querySelector(".caixa-alternativas");
-const caixaPrincipal = document.querySelector(".caixa-resultado");
-const caixaPrincipal = document.querySelector(".caixa-resultado");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".caixa-resultado");
 
 //Array de objetos contendo as perguntas e alernativas
 const perguntas = [
     {
-        enunciado: "Qual a marca líder em dispositivos?"
-    alternativas: [
+        enunciado: "Qual a marca líder em dispositivos?",
+        alternativas: [
             "samsung",
             "apple"
         ],
         correta: 0 // A segunda alternativa é a correta
     },
     {
-        enunciado: "Qual região do Brasil é mais quente?"
-    alternativas: [
+        enunciado: "Qual região do Brasil é mais quente?",
+        alternativas: [
             "Norte",
             "Nordeste"
         ],
         correta: 0 // A segunda alternativa é a correta
     },
     {
-        enunciado: "Qual a comida mais típica do Brasil?"
-    alternativas: [
+        enunciado: "Qual a comida mais típica do Brasil?",
+        alternativas: [
             "açaí",
             "feijoada"
         ],
         correta: 0 // A segunda alternativa é a correta
     },
     {
-        enunciado: "Qual a região mais fria do Brasil?"
-    alternativas: [
+        enunciado: "Qual a região mais fria do Brasil?",
+        alternativas: [
             "Sul",
             "Sudeste"
         ],
         correta: 1 // A segunda alternativa é a correta
     },
     {
-        enunciado: "Qual o país mais populoso?"
-    alternativas: [
+        enunciado: "Qual o país mais populoso?",
+        alternativas: [
             "China",
             "japão"
         ],
@@ -82,6 +82,23 @@ let pontuacao = 0;
     caixaResultado.style.display = "block";
 
     setTimeout(() =>caixaResultado.classList.add("mostrar"),10);
-    textoResultado.textContent = `"Voce acertou $ {pontuacao} de $ {perguntas.lenght} perguntas´;
- }
+    textoResultado.textContent = `"Voce acertou $ {pontuacao} de $ {perguntas.lenght} perguntas`;
+    const botaoReiniciar = document.createElement("button");
+    botaoReiniciar.textContent = "Reiniciar";
+
+    botaoReiniciar.addEventListener("click",() => {
+         atual = 0;
+         pontuacao = 0;
+         caixaResultado.classList.remove("mostrar");
+         caixaResultado.style.display = "none";
+         caixaPrincipal.style.display = "block";
+         mostrarPergunta();
+} );
+ 
+caixaResultado.innerHTML = "";
+caixaResultado.appendChild(textoResultado);
+caixaResultado.appendChild(botaoReiniciar);
+
+}
+mostrarPergunta();
     
